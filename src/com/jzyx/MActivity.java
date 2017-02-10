78295ab210fd4e51fd13b9c3d452406062b461f5
wangyh <344204552@qq.com>
2017-02-10 11:24:01 +0800

    Fri Feb 10 11:24:02     2017

---
+++ src/com/jzyx/MActivity.java
@@ -0,0 +1,10 @@
+package com.jzyx;
+import android.app.Activity;
+import android.os.Bundle;
+public class MActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle ctx){
+		super.onCreate(ctx);
+    }
+
+}
